9488453137d9a05d5f5331e05a9f96d51d65c288
yukimaruyama2023 <yukikeio2023@sslab.ics.keio.ac.jp>
2025-10-26 02:36:26 +0900

    netdata configuration goes well

--- scripts/experiment-sample.py
+++ scripts/experiment-sample.py
@@ -9,15 +9,26 @@ import sys
 
 num_memcacheds = [1,5,10]
 
-conf_root = "../conf"
+conf_root = "./conf"
 
-netdata_conf = f"{conf_root}/{str(num_memcacheds[0]).zfill(3)}mcd/netdata.conf"
-subprocess.run(f"sudo netdata -c {netdata_conf} -D".split())
+def run_memcached(num_memcached):
+    print("hello")
 
-# for num_memcached in num_memcacheds:
-# 	netdata_conf = f"{conf_root}/{str(num_memcached).zfill(3)}mcd/netdata.conf"
-# 	subprocess.run(f"netdata -c {netdata_conf} -D".split())
-#     subprocess.run("sudo pkill netdata".split())
+def run_netdata(num_memcached):
+    memcached_conf = f"{conf_root}/{str(num_memcached).zfill(3)}mcd/go.d/memcached.conf"
+    print(f"{memcached_conf}")
+    subprocess.run(f"sudo cp {memcached_conf} /etc/netdata/go.d/memcached.conf".split())
+    subprocess.run(f"sudo systemctl restart netdata".split())
 
-	######################
+def run_client():
+    print("hello")
 
+def main():
+    for num_memcached in num_memcacheds:
+        run_memcached(num_memcached)
+        run_netdata(num_memcached)
+        run_client()
+        time.sleep(10)
+
+if __name__ == "__main__":
+    main()
