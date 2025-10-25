71f619c8c7fd6831a6d9568a38268ce203c2cf95
yukimaruyama2023 <yukikeio2023@sslab.ics.keio.ac.jp>
2025-10-25 22:01:59 +0900

    able to run netdata

--- scripts/experiment-sample.py
+++ scripts/experiment-sample.py
@@ -1,10 +1,23 @@
+import subprocess
+import atexit
+import time
+import os
+import itertools
+import signal
+import sys
+# import numpy as np
+
 num_memcacheds = [1,5,10]
 
-conf_root = f"./conf"
+conf_root = "../conf"
 
-for num_memcached in num_memcacheds:
-	netdata_conf = f"{conf_root}/{num_memcached}mcd/netdata.conf"
-	subprocess.run(f"netdata -c {netdata_conf}".split())
+netdata_conf = f"{conf_root}/{str(num_memcacheds[0]).zfill(3)}mcd/netdata.conf"
+subprocess.run(f"sudo netdata -c {netdata_conf} -D".split())
+
+# for num_memcached in num_memcacheds:
+# 	netdata_conf = f"{conf_root}/{str(num_memcached).zfill(3)}mcd/netdata.conf"
+# 	subprocess.run(f"netdata -c {netdata_conf} -D".split())
+#     subprocess.run("sudo pkill netdata".split())
 
 	######################
 
