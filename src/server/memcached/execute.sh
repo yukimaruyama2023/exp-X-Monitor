afad0791e743845b560eee22d2de575a8cf30d75
yukimaruyama2023 <yukikeio2023@sslab.ics.keio.ac.jp>
2025-10-26 19:16:59 +0900

    correctly configure netdata and memcached

---
+++ src/server/memcached/execute.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+BASE_PORT=11211
+
+NUM_INSTANCES=${1:-10}
+
+for i in $(seq 0 $((NUM_INSTANCES - 1))); do
+  PORT=$((BASE_PORT + i))
+  taskset --cpu-list 1,2,3,4,5 ./src/server/memcached/memcached -p $PORT -t 1 &
+  # ./memcached -p $PORT -t 1 &
+  echo "Started memcached on port $PORT"
+done
