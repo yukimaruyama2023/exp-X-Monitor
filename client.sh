43c66f9244591b761f0037497aff16b7789d0a95
yukimaruyama2023 <yukikeio2023@sslab.ics.keio.ac.jp>
2025-10-26 15:15:46 +0000

    modify Monitoring_Client and update client.sh

--- client.sh
+++ client.sh
@@ -3,3 +3,8 @@
 # mutilate
 sudo apt-get install scons libevent-dev gengetopt libzmq3-dev
 cd ./src/client/mutilate/ && scons
+
+# monitoring client
+cd -
+gcc -o ./src/client/Monitoring_Client/client_netdata ./src/client/Monitoring_Client/client_netdata.c
+gcc -o ./src/client/Monitoring_Client/client_x-monitor ./src/client/Monitoring_Client/client_x-monitor.c
