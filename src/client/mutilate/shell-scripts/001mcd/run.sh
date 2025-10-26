6f3bffdd4c2e179a174091af155c8e8ff9613417
yukimaruyama2023 <yukikeio2023@sslab.ics.keio.ac.jp>
2025-10-26 23:04:32 +0900

    mutilate script to fullpath

--- src/client/mutilate/shell-scripts/001mcd/run.sh
+++ src/client/mutilate/shell-scripts/001mcd/run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-./mutilate \
+# ./mutilate \
+#   -s 10.10.10.1:11211 \
+#   -T 48 -c 1 -q 0 -u 0 -i exponential:1 -t 80 --noload --depth 1 --records 1000000
+
+/home/maruyama/workspace/exp-X-Monitor/src/client/mutilate/shell-scripts/001mcd/mutilate \
   -s 10.10.10.1:11211 \
   -T 48 -c 1 -q 0 -u 0 -i exponential:1 -t 80 --noload --depth 1 --records 1000000
--- src/client/mutilate/shell-scripts/005mcd/load.sh
+++ src/client/mutilate/shell-scripts/005mcd/load.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-./mutilate \
+# ./mutilate \
+#   -s 10.10.10.1:11211 \
+#   -s 10.10.10.1:11212 \
+#   -s 10.10.10.1:11213 \
+#   -s 10.10.10.1:11214 \
+#   -s 10.10.10.1:11215 \
+#   -T 48 --records 1000000 --loadonly
+
+/home/maruyama/workspace/exp-X-Monitor/src/client/mutilate/shell-scripts/005mcd/mutilate \
   -s 10.10.10.1:11211 \
   -s 10.10.10.1:11212 \
   -s 10.10.10.1:11213 \
