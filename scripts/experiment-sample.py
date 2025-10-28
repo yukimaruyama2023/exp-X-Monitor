ec2a6b8f37ade849ad766a7f1e1e063875e0bcaa
yukimaruyama2023 <yukikeio2023@sslab.ics.keio.ac.jp>
2025-10-28 17:19:26 +0900

    backup

--- scripts/experiment-sample.py
+++ scripts/experiment-sample.py
@@ -15,13 +15,14 @@ remote_data_root = "/home/maruyama/workspace/exp-X-Monitor/data/"
 conf_root = "./conf"
 
 num_memcacheds = [1,5,10]
+metrics = ["user", "kernel"]
 timestamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 output_dir = f"{remote_data_root}/monitoring_latency/{timestamp}"
 
 def run_memcached(num_memcached):
-    print(f"=== [Start] Running Memcached {num_memcached} instances === ")
+    print(f"=== [Start] Running Memcached {num_memcached} instances ===")
     subprocess.Popen(f"./src/server/memcached/execute.sh {num_memcached}".split())
-    print(f"=== [End] Running Memcached {num_memcached} instances === ")
+    print(f"=== [End] Running Memcached {num_memcached} instances ===")
 
 def stop_mutilate():
     subprocess.run(["ssh", remote_host, "pkill", "mutilate"])
@@ -29,21 +30,22 @@ def stop_mutilate():
 def stop_memcached():
     subprocess.run("sudo pkill memcached".split())
 
-def run_netdata(num_memcached):
-    print(f"=== [Start] Running Netdata {num_memcached} === ")
+def run_netdata(num_memcached, netdata):
+    print(f"=== [Start] Running Netdata {num_memcached} ===")
     memcached_conf = f"{conf_root}/{str(num_memcached).zfill(3)}mcd/go.d/memcached.conf"
     print(f"{memcached_conf}")
     subprocess.run(f"sudo cp {memcached_conf} /etc/netdata/go.d/memcached.conf".split())
+    subprocess.run(f"sudo cp {memcached_conf} /etc/netdata/go.d/memcached.conf".split())
     subprocess.run(f"sudo systemctl restart netdata".split())
-    print(f"=== [End] Running Netdata {num_memcached} === ")
+    print(f"=== [End] Running Netdata {num_memcached} ===")
 
 def run_mutilate(num_memcached):
-    print(f"=== [Start] Running mutilate {num_memcached} === ")
+    print(f"=== [Start] Running mutilate {num_memcached} ===")
     subprocess.run(f"ssh {remote_host} {remote_mutilate_scripts}/{str(num_memcached).zfill(3)}mcd/load.sh".split())
     subprocess.Popen(f"ssh {remote_host} {remote_mutilate_scripts}/{str(num_memcached).zfill(3)}mcd/run.sh".split())
-    print(f"=== [End] Running mutilate {num_memcached} === ")
+    print(f"=== [End] Running mutilate {num_memcached} ===")
 
-def run_monitor(num_memcached):
+def run_monitor(num_memcached, metric):
     print(f"=== [Start] Running monitoring client mcd={num_memcached} === ")
     stdin_input = "1\n1\n"
     cmd = (
@@ -56,13 +58,13 @@ def run_monitor(num_memcached):
     )
     print(f"=== [End] Running monitoring client mcd={num_memcached} === ")
 
-def run_client(num_memcached):
-    run_mutilate(num_memcached)
-    run_monitor(num_memcached)
-
-def run_server(num_memcached):
+def run_server(num_memcached, metric):
     run_memcached(num_memcached)
-    run_netdata(num_memcached)
+    run_netdata(num_memcached, metric)
+
+def run_client(num_memcached, metric):
+    run_mutilate(num_memcached)
+    run_monitor(num_memcached, metric)
 
 def stop_server():
     stop_mutilate()
@@ -76,18 +78,17 @@ def make_output_dir():
     subprocess.run(f"ssh {remote_host} {cmd}".split())
     print(f"=== [End] Creating output_dir: {output_dir} === ")
 
-
 def main():
     make_output_dir()
-    for num_memcached in num_memcacheds:
-        print()
-        print(f"############## Running {num_memcached} servers ##########################")
-        run_server(num_memcached)
-        # time.sleep(10)
-        run_client(num_memcached)
-        stop_server()
-        # needed to pkill memcached completely
-        time.sleep(5)
+    for metric in metrics:
+        for num_memcached in num_memcacheds:
+            print()
+            print(f"############## Running {num_memcached} servers ##########################")
+            run_server(num_memcached, metric)
+            run_client(num_memcached, metric)
+            stop_server()
+            # needed to pkill memcached completely
+            time.sleep(5)
 
 if __name__ == "__main__":
     main()
